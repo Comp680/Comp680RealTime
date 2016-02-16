4699cc463ca40c6ba9b3ece690db4b8460993cd8
World93 <david@grefin.com>
2016-02-15 17:26:24 -0800

    Added Folders

---
+++ Node/chat_program/routes/chat.js
@@ -0,0 +1,17 @@
+/**
+ * New node file
+ */
+var express = require('express');
+var router = express.Router();
+
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+	res.render('chat',{title:'chat'});
+});
+
+
+
+
+module.exports = router;

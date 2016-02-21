6484b98864979c309c81d2dc903f7b6c7b72531c
World93 <david@grefin.com>
2016-02-21 11:55:13 -0800

    Update
    -Added TicTacToe Project
    -Built Simple Game

--- Node/chat_program/controller/chat_socket.js
+++ Node/chat_program/controller/chat_socket.js
@@ -9,9 +9,10 @@ function waiting_rooms(min_room_size, max_room_size) {
 	this.add_to_room = function(user_socker) {
 		if(this.number_in_room >= max_room_size){
 			this.room_waiting[0]++;
+			this.number_in_room = 0;
 		}
-		
-		return this.room_waiting[0];
+		this.number_in_room++;
+		return {'room':this.room_waiting[0],'user_number':this.number_in_room};
 		
 	}
 
@@ -72,6 +73,20 @@ function search_object(obj, value) {
 	}
 }
 
+function emit_new_message(socket, msg, emit_to){
+	var rooms = socket.adapter.rooms;
+	rooms = wait_rooms.find_room_by_socket_id(socket.id, rooms);
+	if(msg['for'].toLowerCase()=='not_me'){
+		socket.broadcast.to(rooms.pop()).emit(emit_to, {
+			'msg' : msg.msg,
+		});
+	}else{
+		socket.to(rooms.pop()).emit(emit_to, {
+			'msg' : msg.msg,
+		});
+	}
+}
+
 var number_in_room = 0;
 var room_number = 0;
 var room_min_size = 2;
@@ -81,14 +96,16 @@ var wait_rooms = new waiting_rooms(room_min_size, room_max_size);
 module.exports = function(io) {
 
 	io.sockets.on('connection', function(socket) {
-		var room_num = wait_rooms.add_to_room(socket);
-		
+		var room_info = wait_rooms.add_to_room(socket);
+		var room_num = room_info.room;
 
 		socket.join(room_num);
 		
 		io.to(socket.id).emit('user connect', {// Send message to user
 													// based on their room
-			'msg' : "You are in Room #" + room_num 
+			'msg' : room_num,
+			'room_number': room_num,
+			'player_number':room_info.user_number
 		});
 
 		socket.on('disconnect', function() {
@@ -96,22 +113,20 @@ module.exports = function(io) {
 			var rooms = wait_rooms.find_room_by_socket_id(socket.id, rooms);
 			if (rooms.length > 0) {
 				io.to(rooms.pop()).emit('user connect', {
-					'msg' : "User Disconnected",
-					'for' : 'everyone'
+					'msg' : "User Disconnected"
 				});
 			} else {// Send failure message
 
 			}
 		});
 
+		socket.on('game message',function(msg){//Emit a game message
+			emit_new_message(socket, msg,'game message');
+		});
+		
 		socket.on('chat message', function(msg) {
-			var rooms = socket.adapter.rooms;
-			rooms = wait_rooms.find_room_by_socket_id(socket.id, rooms);
-
-			io.to(rooms.pop()).emit('chat message', {
-				'msg' : msg.msg,
-				'for' : 'everyone'
-			});
+			emit_new_message(socket, msg,'chat message');
+			
 		});
 	});
 

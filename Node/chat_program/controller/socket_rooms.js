69df05b9fb8a936bad8a01cc71790dcde2d0885e
World93 <david@grefin.com>
2016-03-02 09:03:02 -0800

    Removed Useless Code

--- Node/chat_program/controller/socket_rooms.js
+++ Node/chat_program/controller/socket_rooms.js
@@ -20,28 +20,8 @@ module.exports = function waiting_rooms(min_room_size, max_room_size) {
 	this.find_room_by_socket_id = function(socket,socket_id, rooms) {// Find the room
 																// the user
 																// resides in
-		console.log(socket.adapter.sids[socket_id]);
+		//console.log(socket.adapter.sids[socket_id]);
 		return Object.keys(socket.adapter.sids[socket_id]);
-		/*var return_value = [];
-		return_value.push(-1);
-		for ( var key in rooms) {
-			if (typeof rooms[key] === 'object') {
-				if (search_object_key(rooms[key].sockets, socket_id) === true) {
-
-					if (null !== rooms[key].length
-							&& rooms[key].length >= min_room_size) {
-						return_value.push(key);
-					} else {
-						// Current_socket_room
-						return_value[0] = key;// Only room the user is in is
-												// his own
-					}
-
-				}
-
-			}
-		}
-		return return_value;*/
 
 	}
 
